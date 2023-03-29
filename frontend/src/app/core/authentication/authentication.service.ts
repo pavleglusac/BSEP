78d49c49a2d1375be083eb7ba38286e82dc63fd8
Milan <76664400+milsek@users.noreply.github.com>
2023-03-29 03:26:50 +0200

    Implement login (#2)
    * Implement login page design
    
    * Connect login back and front
    
    * Remove unused import

---
+++ frontend/src/app/core/authentication/authentication.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { axiosPublic } from "src/common/axiosPublic";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor() { }
+
+  setJWT(data: { accessToken: string, expiresAt: string }): void {
+    window.localStorage.setItem('jwt', JSON.stringify(data));
+  }
+
+  getToken(): string | null {
+    const jwt = localStorage.getItem('jwt');
+    if (!jwt) return null;
+    return JSON.parse(jwt).accessToken;
+  }
+
+  async login(email: string, password: string): Promise<boolean> {
+    const successfulLogin = await axiosPublic
+      .post('/api/auth/login', {
+        email,
+        password,
+      })
+      .then((res) => {
+        if (res.data) {
+          this.setJWT(res.data);
+          return true;
+        } else {
+          return false;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        return false;
+      });
+    return successfulLogin;
+  }
+}
